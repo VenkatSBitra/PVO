a92b02e39aff84fda489e8c26b9d49cb50092648
Venkat Suprabath Bitra <53979144+VenkatSBitra@users.noreply.github.com>
2024-02-02 17:59:42 +0530

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -1,2 +1,3 @@
 docker rm pvo
-docker run --gpus all -it --name pvo -v /home:/home --ipc=host pvo_running:latest bash
+# docker run --gpus all -it --name pvo -v /home:/home --ipc=host pvo_running:latest bash
+docker run --gpus all -it --name pvo -v /home:/home --ipc=host venkatsbitra/pvo:latest bash
